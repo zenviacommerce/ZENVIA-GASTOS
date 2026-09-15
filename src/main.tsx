cbe53f31a1b99ecd7329eda8500b5467e46e406e
zenviacommerce <info@zenviacommerce.com>
2026-09-15 11:32:24 +0200

    Load order status styles

--- src/main.tsx
+++ src/main.tsx
@@ -20,4 +20,5 @@ import './sales-config.css';
 import './master-data.css';
 import './supplier-period.css';
 import './orders.css';
+import './orders-status.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
