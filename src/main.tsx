9a3d68414de70926e7f203f00299e90b1572f74b
zenviacommerce <info@zenviacommerce.com>
2026-09-15 13:22:28 +0200

    Load order tracking styles

--- src/main.tsx
+++ src/main.tsx
@@ -23,5 +23,6 @@ import './orders.css';
 import './orders-status.css';
 import './orders-edit.css';
 import './orders-products.css';
+import './orders-tracking.css';
 import './dashboard-summary.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
