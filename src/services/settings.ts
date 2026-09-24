b0fc1035962d72fe2b15436f35520876789d12dd
zenviacommerce <info@zenviacommerce.com>
2026-09-25 00:10:24 +0200

    feat: merge dismissed alerts safely in user preferences

--- src/services/settings.ts
+++ src/services/settings.ts
@@ -49,6 +49,7 @@ export function mergeUserPreferencesPatch(current:UserPreferences,patch:UserPref
     filters:patch.filters?{...current.filters,...patch.filters}:current.filters,
     tableColumns:patch.tableColumns?{...current.tableColumns,...patch.tableColumns}:current.tableColumns,
     tableColumnOrder:patch.tableColumnOrder?{...current.tableColumnOrder,...patch.tableColumnOrder}:current.tableColumnOrder,
+    dismissedAlerts:patch.dismissedAlerts?{...current.dismissedAlerts,...patch.dismissedAlerts}:current.dismissedAlerts,
   };
 }
 
